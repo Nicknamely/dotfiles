82c113f1173f478b0244165246c2f52968749cd9
ndiaga <169577450+Nicknamely@users.noreply.github.com>
2025-06-20 22:24:33 +0000

    feat: Add new plugin for cmake and other

--- _brains/nvim/lua/plugins/disabled.lua
+++ _brains/nvim/lua/plugins/disabled.lua
@@ -2,4 +2,5 @@ return {
   { "coffebar/neovim-project", enabled = false },
   -- { "3rd/image.nvim", enabled = false },
   { "elkowar/yuck.vim", enabled = false },
+  { "Civitasv/cmake-tools.nvim", enabled = false },
 }
